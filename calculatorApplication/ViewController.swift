a66df81a227ba54e5ad5b860e23b66ddea182586
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-03-31 21:25:40 +0900

    LV2 구현

--- calculatorApplication/ViewController.swift
+++ calculatorApplication/ViewController.swift
@@ -11,6 +11,8 @@ import SnapKit
 class ViewController: UIViewController {
     
     let label = UILabel()
+    let button = UIButton()
+    let horizontalStackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,31 @@ class ViewController: UIViewController {
     }
 
     func configureUI() {
-        view.addSubview(label)
+        [label, button, horizontalStackView].forEach {
+            view.addSubview($0)
+        }
         label.textColor = .white
         label.text = "12345"
         label.textAlignment = .right
         label.font = UIFont.boldSystemFont(ofSize: 60)
+        
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.backgroundColor = .black
+        horizontalStackView.spacing = 10
+        horizontalStackView.distribution = .fillEqually
+       
+        let buttonTitles = ["7", "8", "9", "+"]
+        for title in buttonTitles {
+            let button = UIButton()
+            button.setTitle(title, for: .normal)
+            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            button.setTitleColor(.white, for: .normal)
+            button.layer.cornerRadius = 40
+            
+            button.snp.makeConstraints { $0.width.height.equalTo(80) }
+            horizontalStackView.addArrangedSubview(button)
+        }
     }
     
     func setConstraints() {
@@ -32,6 +54,12 @@ class ViewController: UIViewController {
             $0.leading.trailing.equalToSuperview().inset(30)
             $0.top.equalToSuperview().offset(200)
             $0.height.equalTo(100)
+        }
+        
+        horizontalStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.top.equalTo(label.snp.bottom).offset(20)
+            $0.height.equalTo(80)
         }
     }
     
