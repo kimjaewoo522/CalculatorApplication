1a4e2b2b6aaea03f76cfb36e5ec7bb1c102ac0de
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-04-01 19:55:09 +0900

    LV3 구현

--- calculatorApplication/ViewController.swift
+++ calculatorApplication/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let label = UILabel()
     let button = UIButton()
     let horizontalStackView = UIStackView()
+    let verticalStackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
     }
 
     func configureUI() {
-        [label, button, horizontalStackView].forEach {
+        [label, button, horizontalStackView, verticalStackView].forEach {
             view.addSubview($0)
         }
         label.textColor = .white
@@ -30,22 +31,37 @@ class ViewController: UIViewController {
         label.textAlignment = .right
         label.font = UIFont.boldSystemFont(ofSize: 60)
         
-        horizontalStackView.axis = .horizontal
-        horizontalStackView.backgroundColor = .black
-        horizontalStackView.spacing = 10
-        horizontalStackView.distribution = .fillEqually
+        verticalStackView.axis = .vertical
+        verticalStackView.backgroundColor = .black
+        verticalStackView.spacing = 10
+        verticalStackView.distribution = .fillEqually
        
-        let buttonTitles = ["7", "8", "9", "+"]
-        for title in buttonTitles {
-            let button = UIButton()
-            button.setTitle(title, for: .normal)
-            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
-            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            button.setTitleColor(.white, for: .normal)
-            button.layer.cornerRadius = 40
-            
-            button.snp.makeConstraints { $0.width.height.equalTo(80) }
-            horizontalStackView.addArrangedSubview(button)
+        let buttonTitles = [
+            ["7", "8", "9", "+"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "4"],
+            ["AC", "0", "=", "/"]
+            ]
+        
+        for row in buttonTitles {
+            let horizontalStackView = UIStackView()
+            horizontalStackView.axis = .horizontal
+            horizontalStackView.backgroundColor = .black
+            horizontalStackView.spacing = 10
+            horizontalStackView.distribution = .fillEqually
+                
+            for title in row {
+                let button = UIButton()
+                button.setTitle(title, for: .normal)
+                button.titleLabel?.font = .boldSystemFont(ofSize: 30) // button.font = UIfont.boldsystemFont(ofSize: 30)는 왜안되는지
+                button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+                button.setTitleColor(.white, for: .normal)
+                button.layer.cornerRadius = 40
+                
+                button.snp.makeConstraints { $0.width.height.equalTo(80) }
+                horizontalStackView.addArrangedSubview(button)
+            }
+            verticalStackView.addArrangedSubview(horizontalStackView)
         }
     }
     
@@ -57,9 +73,12 @@ class ViewController: UIViewController {
         }
         
         horizontalStackView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(30)
-            $0.top.equalTo(label.snp.bottom).offset(20)
             $0.height.equalTo(80)
+        }
+        verticalStackView.snp.makeConstraints {
+            $0.width.equalTo(350)
+            $0.top.equalTo(label.snp.bottom).offset(60)
+            $0.centerX.equalToSuperview()
         }
     }
     
